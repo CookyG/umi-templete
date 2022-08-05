e648bfa32a97131b30d7d1fca21aa3bf92c44f5b
cookygg <379360444@qq.com>
2022-08-05 14:00:27 +0800

    chore: update config

--- config/config.ts
+++ config/config.ts
@@ -1,40 +1,74 @@
 import { defineConfig } from '@umijs/max'
 
-//路由相关配置
+// 路由相关配置
 import Routes from '../router'
 
-//主题相关设置
+// 主题相关设置
 import Theme from './theme'
 
-//antd 全局配置
+// antd 全局配置
 import AntdBase from './antd'
 
+// proxy 相关配置
+import proxy from './proxy'
+
+// docker 相关配置
+const DOCKERFILES = [
+  'config/docker/Dockerfile',
+  'config/docker/docker-compose.yml',
+  'config/docker/Deployment-service-Ingress.yml',
+]
+
+const { REACT_APP_ENV } = process.env
+
+const PROD = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
+  // 路由路径
   base: '/',
+
+  // 资源路径
+  publicPath: '/',
+
   hash: true,
+
+  title: 'Ashermed',
+
   locale: {
     antd: true,
     default: 'zh-CN',
     baseNavigator: true,
   },
+
   antd: AntdBase,
+
   access: {},
+
   model: {},
+
   initialState: {},
+
   request: {},
 
   routes: Routes,
+
+  proxy: PROD ? {} : proxy[REACT_APP_ENV || 'dev'],
 
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: Theme,
 
   npmClient: 'pnpm',
 
-  // tailwindcss: {},
+  tailwindcss: {},
 
   // unocss 配置
-  plugins: [require.resolve('@umijs/plugins/dist/unocss')],
-  unocss: {
-    watch: ['./src/pages/**/*.tsx', './src/components/**.tsx', './src/layouts/**.tsx'], // 添加其他包含 unocss 的 classname 的文件目录
-  },
+  // plugins: [require.resolve('@umijs/plugins/dist/unocss')],
+  // unocss: {
+  //   watch: ['./src/pages/**/*.tsx', './src/components/**.tsx', './src/layouts/**.tsx'], // 添加其他包含 unocss 的 classname 的文件目录
+  // },
+
+  // 检测未使用的文件和导出
+  deadCode: {},
+
+  copy: [...DOCKERFILES],
 })
